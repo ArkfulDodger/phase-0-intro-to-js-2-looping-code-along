9aeb04fc9e6aaee84ba770e2f2bfefe2998118b7
Noah Reece <noah.w.reece@gmail.com>
2022-02-25 09:41:00 -0500

    commit tree e33b4e6d1d707c2566bbe699c058bdc136043701

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        const name = names[i];
+        messages.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+
+    return messages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
